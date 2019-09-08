3229d8e6c8573c4f93cac4532c17207f0ff623bc
Sindri Ingólfsson <sindriingolfsson@gmail.com>
2019-09-08 20:51:40 +0000

    almost done

--- main.go
+++ main.go
@@ -3,20 +3,15 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"regexp"
 	"strconv"
+	"strings"
 )
 
-func main() {
-	fmt.Println("Hello World")
-	game := initializeBoard()
-
-}
+var allBlock = [9]bool{true, true, true, true, true, true, true, true, true}
 
 type cell struct {
-	blocked  [9]bool //Which numbers can't be placed
-	exploded bool    //Have we used this value for removing possibilities
-	value    int
+	blocked [9]bool //Which numbers can't be placed
+	value   int
 }
 
 type sudoku struct {
@@ -24,10 +19,154 @@ type sudoku struct {
 	filled int //How many total squares filled
 }
 
+func main() {
+	fmt.Println("Hello World")
+	game := initializeBoard("presets/board1.txt")
+	game.printBoard()
+	game.printBlock()
+
+	game.firstSweep()
+	game.printBoard()
+	game.printBlock()
+
+	progress := game.solve()
+	progress.printBoard()
+	progress.printBlock()
+
+}
+
+//Attempts to solve the board, cannot make guesses
+func (s sudoku) solve() sudoku {
+
+	for {
+		for i := 0; i < 9; i++ {
+			for j := 0; j < 9; j++ {
+				if s.isFree(i, j) {
+					// Check if there is only one available value
+					if val := s.checkOneVal(i, j); val != 0 {
+						s.explode(i, j, val)
+					}
+					// Check row
+					if val := s.checkRow(i, j); val != 0 {
+						s.explode(i, j, val)
+					}
+
+					// Check column
+					if val := s.checkCol(i, j); val != 0 {
+						s.explode(i, j, val)
+					}
+
+					//Check inner block
+					if val := s.checkChunk(i, j); val != 0 {
+						s.explode(i, j, val)
+					}
+
+				}
+			}
+		}
+
+	}
+	return s
+}
+
+//Check if this cell has only one possible value left
+func (s *sudoku) checkOneVal(x int, y int) int {
+	var last, sum int
+	for i, blocked := range s.board[x][y].blocked {
+		if !blocked {
+			sum++
+			last = i
+		}
+		if sum > 1 {
+			return 0
+		}
+	}
+	return last + 1
+}
+
+//Check if I'm the only one in my row who can receive a specific value
+func (s *sudoku) checkRow(x int, y int) int {
+	var success bool
+	for v, block := range s.board[x][y].blocked {
+		if !block {
+			success = true
+			for i := 0; i < 9; i++ {
+				if !s.board[x][i].blocked[v] && i != y {
+					//Another cell can have this value
+					success = false
+					break
+				}
+			}
+			if success {
+				return v + 1
+			}
+		}
+	}
+	return 0
+}
+
+//Check if I'm the only one in my 3x3 chunk who can receive a specific value
+func (s *sudoku) checkChunk(x int, y int) int {
+	xn := x - (x % 3)
+	yn := y - (y % 3)
+	var success bool
+	for v, block := range s.board[x][y].blocked {
+		if !block {
+			success = true
+			for i := xn; i < xn+3; i++ {
+				for j := yn; j < yn+3; j++ {
+					if !s.board[i][j].blocked[v] && (i != x || j != y) {
+						//Another cell can have this value
+						success = false
+						break
+					}
+				}
+
+			}
+			if success {
+				return v + 1
+			}
+		}
+	}
+	return 0
+}
+
+//Check if I'm the only one in my column who can receive a specific value
+func (s *sudoku) checkCol(x int, y int) int {
+	var success bool
+	for v, block := range s.board[x][y].blocked {
+		if !block {
+			success = true
+			for i := 0; i < 9; i++ {
+				if !s.board[i][y].blocked[v] && i != x {
+					//Another cell can have this value
+					success = false
+					break
+				}
+			}
+			if success {
+				return v + 1
+			}
+		}
+	}
+	return 0
+}
+func (s *sudoku) firstSweep() {
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			if curr := s.board[i][j].value; curr != 0 {
+				s.explode(i, j, curr)
+			}
+		}
+	}
+}
+
 func (s *sudoku) explode(x int, y int, val int) {
-	s.board[x][y].exploded = true
+	s.board[x][y].blocked = allBlock
+	s.filled++
 
 	s.board[x][y].value = val
+	val-- //indexing is 0-8 while sudoku is 1-9
 	s.explodeInner(x, y, val)
 	s.explodeVertical(x, val)
 	s.explodeHorizontal(y, val)
@@ -38,8 +177,8 @@ func (s *sudoku) explodeInner(x int, y int, val int) {
 	x = x - (x % 3)
 	y = y - (y % 3)
 
-	for i := 0; i < 3; i++ {
-		for j := 0; j < 3; j++ {
+	for i := x; i < x+3; i++ {
+		for j := y; j < y+3; j++ {
 			if s.board[i][j].blocked[val] == false {
 				s.board[i][j].blocked[val] = true
 			}
@@ -63,23 +202,54 @@ func (s *sudoku) explodeHorizontal(y int, val int) {
 	}
 }
 
-func initializeBoard(path string) sudoku {
-	Sudoku := sudoku{readBoard(path), 0}
-	return board
+func (s *sudoku) isFree(x int, y int) bool {
+	return s.board[x][y].value == 0
 }
 
-func (s sudoku)printBoard() {
-	var vals [9]interface{}
-	for i:=0; i<9; i++ {
-		for j:=0; j<9; j++{
-			vals = Append(s.board[i][j].value)
-		}
+func initializeBoard(path string) sudoku {
+	Sudoku := sudoku{readBoard(path), 0}
+	return Sudoku
+}
+
+func (s sudoku) printBoard() {
+	for i := 0; i < 9; i++ {
 		if i%3 == 0 {
-			fmt.Println("-------------")
+			fmt.Println("-------------------------")
 		}
-		fmt.Printf{"|%d%d%d|%d%d%d|%d%d%d|", vals...}
+		for j := 0; j < 9; j++ {
+			if j%3 == 0 {
+				fmt.Printf("| ")
+			}
+			fmt.Printf("%d ", s.board[i][j].value)
+		}
+		fmt.Printf("|\n")
 	}
-	fmt.Println("-------------")
+	fmt.Println("-------------------------")
+}
+
+func (s sudoku) printBlock() {
+	for i := 0; i < 9; i++ {
+		if i%3 == 0 {
+			fmt.Println("")
+		}
+		for j := 0; j < 9; j++ {
+			if j%3 == 0 {
+				fmt.Printf("| ")
+			}
+			var form [9]int
+			for index, value := range s.board[i][j].blocked {
+				if !value {
+					form[index] = index + 1
+				}
+			}
+			for _, value := range form {
+				fmt.Print(value)
+			}
+			fmt.Print(" ")
+		}
+		fmt.Print("|\n")
+	}
+	fmt.Println()
 }
 
 func readBoard(path string) [9][9]cell {
@@ -89,15 +259,16 @@ func readBoard(path string) [9][9]cell {
 	check(err)
 
 	//Split the input file on commas and newlines
-	rule := regexp.MustCompile("\n,")
-	list := rule.Split(string(dat), -1)
+	f := func(c rune) bool {
+		return c == '\n' || c == ','
+	}
+	list := strings.FieldsFunc(string(dat), f)
 
-	c := 0
 	var value int
 	for i, c := 0, 0; i < 9; i++ {
 		for j := 0; j < 9; j, c = j+1, c+1 {
 			value, err = strconv.Atoi(list[c])
-			board[i][j] = cell{noBlock, false, value}
+			board[i][j] = cell{noBlock, value}
 		}
 	}
 	return board
